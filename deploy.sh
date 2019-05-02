52254b650e1a97e89dee84d8e4b52f90a5bd6024
Steven Lim <stevenlim890@gmail.com>
2019-05-01 18:37:02 -0700

    Add logging and pull prior to push

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
+echo "Logging out of gigalixir account"
+
 gigalixir logout
+
+echo "Starting deployment"
 
 while IFS=',' read -r email password directory || [[ -n "$directory" ]]; 
 do
@@ -6,9 +10,12 @@ do
 
 	gigalixir account
 
-	cd "$directory" && git commit --allow-empty -m "deploy" && 
+	cd "$directory" && git pull gigalixir master && 
+		git commit --allow-empty -m "deploy" && 
 		`git push gigalixir master`
 
 	gigalixir logout
 
-done < "$1"
+done < "$1"	
+
+echo "Deployment finished"
